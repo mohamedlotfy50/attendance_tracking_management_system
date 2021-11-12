1e146a8552e5cf28235838d8fe707ab9862cf03d
nada <nadooinasef1@gmail.com>
2021-11-13 01:42:57 +0200

    fragments

--- app/src/main/java/com/example/attendance_tracking_management_system/HomeActivity.java
+++ app/src/main/java/com/example/attendance_tracking_management_system/HomeActivity.java
@@ -1,14 +1,55 @@
 package com.example.attendance_tracking_management_system;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    BottomNavigationView navBar;
+    private HomeFragment homeFragment;
+    private EmployeesFragment employeesFragment;
+    private SettingsFragment settingsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        navBar = findViewById(R.id.bottomNavView);
+
+        homeFragment = new HomeFragment();
+        employeesFragment = new EmployeesFragment();
+        settingsFragment = new SettingsFragment();
+
+
+        InitializeFragment(homeFragment);
+        navBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        InitializeFragment(homeFragment);
+                        return true;
+                    case R.id.nav_employees:
+                        InitializeFragment(employeesFragment);
+                        return true;
+                    case R.id.nav_settings:
+                        InitializeFragment(settingsFragment);
+                        return true;
+            }
+            return true;
+        }});
+    }
+    private void InitializeFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/example/attendance_tracking_management_system/MainActivity.java
+++ app/src/main/java/com/example/attendance_tracking_management_system/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.attendance_tracking_management_system;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = findViewById(R.id.loginBtn);
+
+
+        Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
     }
 }
