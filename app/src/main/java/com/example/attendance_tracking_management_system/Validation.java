f28c00055fd009d2727977c42800acbc9d8c0ed6
Mohamed Lotfy <molotfy50@gmail.com>
2021-12-16 00:45:36 +0200

    finish the login

---
+++ app/src/main/java/com/example/attendance_tracking_management_system/Validation.java
@@ -0,0 +1,28 @@
+package com.example.attendance_tracking_management_system;
+
+import java.util.regex.Pattern;
+
+public class Validation {
+
+    public static boolean isValidEmail(String email){
+        if(Pattern.matches("^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\\.[a-zA-Z]+$", email)){
+
+            return true;
+        }
+
+
+        return  false;
+    }
+
+    public static boolean isValidPassword(String password){
+        if(password.length()>=6){
+
+
+            return true;
+        }
+
+
+        return  false;
+    }
+
+}
