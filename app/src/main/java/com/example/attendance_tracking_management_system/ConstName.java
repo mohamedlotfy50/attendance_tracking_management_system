97ccb457ff077fbab39967f5e328a5d26b72229d
Mohamed Lotfy <molotfy50@gmail.com>
2021-12-24 14:54:29 +0200

    leaving request

--- app/src/main/java/com/example/attendance_tracking_management_system/ConstName.java
+++ app/src/main/java/com/example/attendance_tracking_management_system/ConstName.java
@@ -3,6 +3,8 @@ package com.example.attendance_tracking_management_system;
 public class ConstName {
     final static String sharedPref = "atms";
     final static String user = "user";
+    final static String dbUser = "users";
     final static String attendance = "attendance";
+    final static String leaveRequests = "leave requests";
 
 }
